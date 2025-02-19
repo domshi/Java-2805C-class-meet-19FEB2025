dc7c750aa9586dcb2d0482dca7a6fd5b76e8c140
domshi <90562421+domshi@users.noreply.github.com>
2025-02-19 23:56:19 +0000

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,12 @@ public class Main {
     System.out.println("Testing override, overload, and abstract. \n\n\n\n");
 
     //Robot rOne = new Robot("Roomba", "Mopper");
-    //Robot rTwo = new Robot();
+    Robot rTwo = new Cleaning();
     Cleaning cOne = new Cleaning("Roomba", "Floor Cleaner", "Pine sol");
     Exploration eOne = new Exploration("Mars One", "Mapping", "Mass Spectrometer");
     
     //System.out.println(rOne);
-    //System.out.println(rTwo);
+    System.out.println(rTwo);
     System.out.println(cOne + cOne.move("south", "700MPH"));
     System.out.println(eOne + eOne.move("west", "10MPH"));
     
